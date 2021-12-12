b44b291f35f00b65edd875e5bb1faf2412fb5d65
Iztok <iztok@abelium.com>
2021-12-12 10:19:43 +0100

    64 and 128 bit arithmetic

---
+++ contracts/utils/lib/SafeMath128.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+
+library SafeMath128 {
+    uint256 internal constant MAX_UINT128 = type(uint128).max;
+    int256 internal constant MAX_INT128 = type(int128).max;
+    
+    // downcasting from 256 bit
+    
+    function toUint128(uint256 a) internal pure returns (uint128) {
+        require(a <= MAX_UINT128, "SafeMath128: conversion overflow");
+        return uint128(a);
+    }
+
+    // signed/unsigned conversion
+    
+    function toUint128(int256 a) internal pure returns (uint128) {
+        require(a >= 0, "SafeMath128: negative unsigned");
+        require(a <= int256(MAX_UINT128), "SafeMath128: conversion overflow");
+        return uint128(a);
+    }
+
+    function toInt128(uint256 a) internal pure returns (int128) {
+        require(a <= uint256(MAX_INT128), "SafeMath128: conversion overflow");
+        return int128(a);
+    }
+    
+    // 128 bit arithmetic
+    
+    function add128(uint128 a, uint128 b) internal pure returns (uint128) {
+        uint256 c = uint256(a) + uint256(b);
+        require(c <= MAX_UINT128, "SafeMath128: addition overflow");
+        return uint128(c);
+    }
+
+    function sub128(uint128 a, uint128 b, string memory message) internal pure returns (uint128) {
+        require(a >= b, message);
+        uint256 c = uint256(a) - uint256(b);
+        return uint128(c);
+    }
+    
+    function mul128(uint128 a, uint128 b) internal pure returns (uint128) {
+        uint256 c = uint256(a) * uint256(b);    // fits into 256 bits
+        require(c <= MAX_UINT128, "SafeMath128: mul overflow");
+        return uint128(c);
+    }
+
+    // functions that simultaneously add/sub and cast from 256 bits to 128 bits
+        
+    function add128(uint256 a, uint256 b) internal pure returns (uint128) {
+        uint256 c = a + b;
+        require(a <= MAX_UINT128 && b <= MAX_UINT128 && c <= MAX_UINT128, "SafeMath128: addition overflow");
+        return uint128(c);
+    }
+
+    function sub128(uint256 a, uint256 b, string memory message) internal pure returns (uint128) {
+        require(a >= b, message);
+        uint256 c = a - b;
+        require(c <= MAX_UINT128, "SafeMath128: sub above 128bit");
+        return uint128(c);
+    }
+
+    // cheaper version of safe mulDiv - no need for handling uint256 overflow
+    
+    function mulDiv(uint128 a, uint128 mul, uint256 div) internal pure returns (uint256) {
+        require(div != 0, "SafeMath128: mulDiv by zero");
+        return (uint256(a) * uint256(mul)) / div; // intermediate values limited to 256 bits
+    }
+}
--- contracts/utils/lib/SafeMath64.sol
+++ contracts/utils/lib/SafeMath64.sol
@@ -3,13 +3,56 @@ pragma solidity 0.7.6;
 
 
 library SafeMath64 {
-    uint256 internal constant MAX_UINT64 = (1 << 64) - 1;
+    uint256 internal constant MAX_UINT64 = type(uint64).max;
+    int256 internal constant MAX_INT64 = type(int64).max;
+    
+    // downcasting from 256 or 128 bit
     
     function toUint64(uint256 a) internal pure returns (uint64) {
         require(a <= MAX_UINT64, "SafeMath64: conversion overflow");
         return uint64(a);
     }
 
+    function toUint64(uint128 a) internal pure returns (uint64) {
+        require(a <= MAX_UINT64, "SafeMath64: conversion overflow");
+        return uint64(a);
+    }
+
+    // signed/unsigned conversion
+    
+    function toUint64(int256 a) internal pure returns (uint64) {
+        require(a >= 0, "SafeMath64: negative unsigned");
+        require(a <= int256(MAX_UINT64), "SafeMath64: conversion overflow");
+        return uint64(a);
+    }
+
+    function toInt64(uint256 a) internal pure returns (int64) {
+        require(a <= uint256(MAX_INT64), "SafeMath64: conversion overflow");
+        return int64(a);
+    }
+    
+    // 64 bit arithmetic
+    
+    function add64(uint64 a, uint64 b) internal pure returns (uint64) {
+        uint256 c = uint256(a) + uint256(b);
+        require(c <= MAX_UINT64, "SafeMath64: addition overflow");
+        return uint64(c);
+    }
+
+    function sub64(uint64 a, uint64 b, string memory message) internal pure returns (uint64) {
+        require(a >= b, message);
+        uint256 c = uint256(a) - uint256(b);
+        return uint64(c);
+    }
+    
+    function mul64(uint64 a, uint64 b) internal pure returns (uint64) {
+        uint256 c = uint256(a) * uint256(b);    // fits into 128 bits
+        require(c <= MAX_UINT64, "SafeMath64: mul overflow");
+        return uint64(c);
+    }
+
+    // functions that add/subtract and cast result from 256 bits to 64 bits
+        
     function add64(uint256 a, uint256 b) internal pure returns (uint64) {
         uint256 c = a + b;
         require(a <= MAX_UINT64 && b <= MAX_UINT64 && c <= MAX_UINT64, "SafeMath64: addition overflow");
@@ -21,5 +64,12 @@ library SafeMath64 {
         uint256 c = a - b;
         require(c <= MAX_UINT64, "SafeMath64: sub above 64bit");
         return uint64(c);
+    }
+
+    // cheaper version of safe mulDiv - no need for handling uint256 overflow
+    
+    function mulDiv(uint64 a, uint64 mul, uint256 div) internal pure returns (uint256) {
+        require(div != 0, "SafeMath64: mulDiv by zero");
+        return (uint256(a) * uint256(mul)) / div; // intermediate values limited to 128 bits
     }
 }
