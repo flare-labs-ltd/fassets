73cd8f1dce88c654caf522cac8b0649168a35f57
Iztok <iztok@abelium.com>
2021-12-13 10:33:13 +0100

    test support

---
+++ test/unit/fasset/AgentVault.ts
@@ -0,0 +1,18 @@
+import { constants } from "@openzeppelin/test-helpers";
+import { getTestFile } from "flare-smart-contracts/test/utils/constants";
+import { WNatInstance } from "../../../typechain-truffle";
+
+const WNat = artifacts.require("WNat");
+const AgentVault = artifacts.require("AgentVault");
+
+contract(`AgentVault.sol; ${getTestFile(__filename)}; Check point unit tests`, async accounts => {
+    let wnat: WNatInstance;
+    
+    beforeEach(async() => {
+        wnat = await WNat.new(accounts[0], "WNat", "WNAT");
+    });
+    
+    it("can create", async () => {
+        const agentVault = await AgentVault.new(constants.ZERO_ADDRESS, wnat.address, accounts[1]);
+    });
+});
