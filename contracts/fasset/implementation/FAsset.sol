4ee6c1f2a7557deea9858dfd93c1a581c2e36a93
Iztok <iztok@abelium.com>
2022-02-28 09:59:19 +0100

    changed flattening, allow typescipt import from node_modules

--- contracts/fasset/implementation/FAsset.sol
+++ contracts/fasset/implementation/FAsset.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
-import "../../../cache/flattened/VPToken.sol";
+import { VPToken } from "../../../cache/flattened/FlareSmartContracts.sol";
 import "../interface/IFAsset.sol";
 
 contract FAsset is IFAsset, VPToken {
--- contracts/fasset/mock/ImportContractsMock.sol
+++ contracts/fasset/mock/ImportContractsMock.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
-import "../../../cache/flattened/WNat.sol";
+import "../../../cache/flattened/FlareSmartContracts.sol";
 
 // should never be use - just 
 contract ImportContractMock {
-    WNat public wnat;
 }
