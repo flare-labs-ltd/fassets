0c786939e15f0bad0470edad8b857be3acbccb12
Urska Krivc <urska.krivc>
2022-04-21 10:35:51 +0200

    another batch of unit tests for AssetManagerController

--- test/unit/fasset/AssetManagerController.ts
+++ test/unit/fasset/AssetManagerController.ts
@@ -2,9 +2,9 @@ import { expectRevert, time, expectEvent } from "@openzeppelin/test-helpers";
 import { AddressUpdaterInstance, AssetManagerControllerInstance, AssetManagerInstance, AttestationClientMockInstance, FAssetInstance, FtsoMockInstance, WNatInstance } from "../../../typechain-truffle";
 import { AssetManagerSettings } from "../../utils/fasset/AssetManagerTypes";
 import { newAssetManager } from "../../utils/fasset/DeployAssetManager";
-import { getTestFile, toBN, toBNExp, toStringExp } from "../../utils/helpers";
+import { getTestFile, HOURS, MINUTES, toBN, toBNExp, toStringExp } from "../../utils/helpers";
 import { setDefaultVPContract } from "../../utils/token-test-helpers";
-import { assertWeb3Equal, web3ResultStruct } from "../../utils/web3assertions";
+import { assertWeb3DeepEqual, assertWeb3Equal, web3ResultStruct } from "../../utils/web3assertions";
 import { createTestSettings } from "./test-settings";
 
 const AttestationClient = artifacts.require('AttestationClientMock');
@@ -60,7 +60,7 @@ contract(`AssetManagerController.sol; ${getTestFile(__filename)}; Asset manager 
         it("should know about governance", async () => {
             const governance_test = await assetManagerController.governance();
             assert.equal(governance, governance_test);
-        }) 
+        })
 
         it("should get asset managers and check if exist", async () => {
             const managers = await assetManagerController.getAssetManagers();
@@ -75,18 +75,17 @@ contract(`AssetManagerController.sol; ${getTestFile(__filename)}; Asset manager 
             let fAsset2: FAssetInstance;
             const managers_current = await assetManagerController.getAssetManagers();
             [assetManager2, fAsset2] = await newAssetManager(governance, assetManagerController.address, "Ethereum", "ETH", 18, settings);
-            
+
             await assetManagerController.addAssetManager(assetManager2.address, { from: governance });
             const managers_add = await assetManagerController.getAssetManagers();
-            assert.equal(managers_current.length+1, managers_add.length);
+            assert.equal(managers_current.length + 1, managers_add.length);
 
             await assetManagerController.removeAssetManager(assetManager2.address, { from: governance });
             const managers_remove = await assetManagerController.getAssetManagers();
             assert.equal(managers_current.length, managers_remove.length);
         });
 
-
-        it("should update whitelist only with governance", async () => {
+        it("should revert updating whitelist without governance", async () => {
             let res = assetManagerController.setWhitelist([assetManager.address], randomAddress());
             await expectRevert(res, "only governance")
         });
@@ -100,11 +99,10 @@ contract(`AssetManagerController.sol; ${getTestFile(__filename)}; Asset manager 
         it("should revert updating lot size when increase or decrease is too big", async () => {
             const currentSettings = await assetManager.getSettings();
             let lotSizeAMG_big = toBN(currentSettings.lotSizeAMG).muln(3);
-            const res_big = assetManagerController.setLotSizeAmg([assetManager.address], lotSizeAMG_big, { from: governance });
             let lotSizeAMG_small = toBN(currentSettings.lotSizeAMG).divn(5);
-            
+            const res_big = assetManagerController.setLotSizeAmg([assetManager.address], lotSizeAMG_big, { from: governance });
             const res_small = assetManagerController.setLotSizeAmg([assetManager.address], lotSizeAMG_small, { from: governance });
-            
+
             await expectRevert(res_big, "lot size increase too big");
             await expectRevert(res_small, "lot size decrease too big")
         });
@@ -112,26 +110,187 @@ contract(`AssetManagerController.sol; ${getTestFile(__filename)}; Asset manager 
         it("should revert updating payment challenge reward when increase or decrease is too big", async () => {
             const currentSettings = await assetManager.getSettings();
             // payment challenge reward could be zero 
-            if(!toBN(currentSettings.paymentChallengeRewardNATWei).eqn(0)) {
+            if (!toBN(currentSettings.paymentChallengeRewardNATWei).eqn(0)) {
                 let paymentChallengeRewardNATWei_big = toBN(currentSettings.paymentChallengeRewardNATWei).muln(5);
                 let paymentChallengeRewardNATWei_small = toBN(currentSettings.paymentChallengeRewardNATWei).divn(5);
-                
-                let res1 =  assetManagerController.setPaymentChallengeReward([assetManager.address], paymentChallengeRewardNATWei_big, currentSettings.paymentChallengeRewardBIPS, { from: governance });
+
+                let res1 = assetManagerController.setPaymentChallengeReward([assetManager.address], paymentChallengeRewardNATWei_big, currentSettings.paymentChallengeRewardBIPS, { from: governance });
                 await expectRevert(res1, "increase too big");
-                let res2 =  assetManagerController.setPaymentChallengeReward([assetManager.address], paymentChallengeRewardNATWei_small, currentSettings.paymentChallengeRewardBIPS, { from: governance });
+                let res2 = assetManagerController.setPaymentChallengeReward([assetManager.address], paymentChallengeRewardNATWei_small, currentSettings.paymentChallengeRewardBIPS, { from: governance });
                 await expectRevert(res2, "decrease too big");
             }
-            
-            if(!toBN(currentSettings.paymentChallengeRewardBIPS).eqn(0)) {
+
+            if (!toBN(currentSettings.paymentChallengeRewardBIPS).eqn(0)) {
                 let paymentChallengeRewardBIPS_big = toBN(currentSettings.paymentChallengeRewardBIPS).add(toBN(100)).muln(5);
                 let paymentChallengeRewardBIPS_small = toBN(currentSettings.paymentChallengeRewardBIPS).divn(5);
-    
-                let res3 =  assetManagerController.setPaymentChallengeReward([assetManager.address], currentSettings.paymentChallengeRewardNATWei, paymentChallengeRewardBIPS_big, { from: governance });
+
+                let res3 = assetManagerController.setPaymentChallengeReward([assetManager.address], currentSettings.paymentChallengeRewardNATWei, paymentChallengeRewardBIPS_big, { from: governance });
                 await expectRevert(res3, "increase too big");
-                let res4 =  assetManagerController.setPaymentChallengeReward([assetManager.address], currentSettings.paymentChallengeRewardNATWei, paymentChallengeRewardBIPS_small, { from: governance });
+                let res4 = assetManagerController.setPaymentChallengeReward([assetManager.address], currentSettings.paymentChallengeRewardNATWei, paymentChallengeRewardBIPS_small, { from: governance });
                 await expectRevert(res4, "decrease too big");
             }
+        });
 
+        it("should update time for payment", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let underlyingBlocksForPayment_new = toBN(currentSettings.underlyingBlocksForPayment).muln(2);
+            let underlyingSecondsForPayment_new = toBN(currentSettings.underlyingSecondsForPayment).muln(2);
+            let res = await assetManagerController.setTimeForPayment([assetManager.address], underlyingBlocksForPayment_new, underlyingSecondsForPayment_new, { from: governance });
+            expectEvent(res, "SettingChangeScheduled", { name: "underlyingBlocksForPayment", value: toBN(underlyingBlocksForPayment_new) });
+            expectEvent(res, "SettingChangeScheduled", { name: "underlyingSecondsForPayment", value: toBN(underlyingSecondsForPayment_new) });
+        });
+
+        it("should revert updating max trusted price age seconds when increase or decrease is too big", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let maxTrustedPriceAgeSeconds_big = toBN(currentSettings.maxTrustedPriceAgeSeconds).muln(60);
+            let maxTrustedPriceAgeSeconds_small = toBN(currentSettings.maxTrustedPriceAgeSeconds).divn(60);
+            let res_big = assetManagerController.setMaxTrustedPriceAgeSeconds([assetManager.address], maxTrustedPriceAgeSeconds_big, { from: governance });
+            let res_small = assetManagerController.setMaxTrustedPriceAgeSeconds([assetManager.address], maxTrustedPriceAgeSeconds_small, { from: governance });
+            await expectRevert(res_big, "fee increase too big");
+            await expectRevert(res_small, "fee decrease too big");
+        });
+
+        it("should update max trusted price age seconds", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let maxTrustedPriceAgeSeconds_new = toBN(currentSettings.maxTrustedPriceAgeSeconds).addn(20);
+            let res = await assetManagerController.setMaxTrustedPriceAgeSeconds([assetManager.address], maxTrustedPriceAgeSeconds_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "maxTrustedPriceAgeSeconds", value: toBN(maxTrustedPriceAgeSeconds_new) });
+        });
+
+        it("should revert updating collateral reservation fee bips when increase or decrease is too big", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let collateralReservationFeeBIPS_big = toBN(currentSettings.collateralReservationFeeBIPS).muln(5);
+            let collateralReservationFeeBIPS_small = toBN(currentSettings.collateralReservationFeeBIPS).divn(5);
+            let res_big = assetManagerController.setCollateralReservationFeeBips([assetManager.address], collateralReservationFeeBIPS_big, { from: governance });
+            let res_small = assetManagerController.setCollateralReservationFeeBips([assetManager.address], collateralReservationFeeBIPS_small, { from: governance });
+            await expectRevert(res_big, "fee increase too big");
+            await expectRevert(res_small, "fee decrease too big");
+        });
+        
+        it("should update collateral reservation fee bips", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let collateralReservationFeeBIPS_new = toBN(currentSettings.collateralReservationFeeBIPS).muln(2);
+            let res = await assetManagerController.setCollateralReservationFeeBips([assetManager.address], collateralReservationFeeBIPS_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "collateralReservationFeeBIPS", value: toBN(collateralReservationFeeBIPS_new) });
+        });
+        
+        it("should revert updating redemption fee bips when increase or decrease is too big", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let redemptionFeeBIPS_big = toBN(currentSettings.redemptionFeeBIPS).muln(5);
+            let redemptionFeeBIPS_small = toBN(currentSettings.redemptionFeeBIPS).divn(5);
+            let res_big = assetManagerController.setRedemptionFeeBips([assetManager.address], redemptionFeeBIPS_big, { from: governance });
+            let res_small = assetManagerController.setRedemptionFeeBips([assetManager.address], redemptionFeeBIPS_small, { from: governance });
+            await expectRevert(res_big, "fee increase too big");
+            await expectRevert(res_small, "fee decrease too big");
+        });
+
+        it("should revert updating confirmation by others after seconds when value too low", async () => {
+            let confirmationByOthersAfterSeconds_small = 1.8 * HOURS;
+            let res_big = assetManagerController.setConfirmationByOthersAfterSeconds([assetManager.address], confirmationByOthersAfterSeconds_small, { from: governance });
+            await expectRevert(res_big, "must be at least two hours");
+        });
+
+        it("should update confirmation by others after seconds", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let confirmationByOthersAfterSeconds_new = toBN(currentSettings.confirmationByOthersAfterSeconds).muln(2);
+            let res = await assetManagerController.setConfirmationByOthersAfterSeconds([assetManager.address], confirmationByOthersAfterSeconds_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "confirmationByOthersAfterSeconds", value: toBN(confirmationByOthersAfterSeconds_new) });
+        });
+
+        it("should revert updating confirmation by others reward NATWei when increase or decrease is too big", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let confirmationByOthersRewardNATWei_big = toBN(currentSettings.confirmationByOthersRewardNATWei).muln(5);
+            let confirmationByOthersRewardNATWei_small = toBN(currentSettings.confirmationByOthersRewardNATWei).divn(5);
+            let res_big = assetManagerController.setConfirmationByOthersRewardNatWei([assetManager.address], confirmationByOthersRewardNATWei_big, { from: governance });
+            let res_small = assetManagerController.setConfirmationByOthersRewardNatWei([assetManager.address], confirmationByOthersRewardNATWei_small, { from: governance });
+            await expectRevert(res_big, "fee increase too big");
+            await expectRevert(res_small, "fee decrease too big");
+        });
+
+        it("should update confirmation by others reward NATWei", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let confirmationByOthersRewardNATWei_new = toBN(currentSettings.confirmationByOthersRewardNATWei).muln(2);
+            let res = await assetManagerController.setConfirmationByOthersRewardNatWei([assetManager.address], confirmationByOthersRewardNATWei_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "confirmationByOthersRewardNATWei", value: toBN(confirmationByOthersRewardNATWei_new) });
+        });
+
+        it("should revert updating max redeemed tickets when increase or decrease is too big or value is < 1", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let maxRedeemedTickets_big = toBN(currentSettings.maxRedeemedTickets).muln(3);
+            let maxRedeemedTickets_small = toBN(currentSettings.maxRedeemedTickets).divn(5);
+            let maxRedeemedTickets_zero = 0;
+
+            let res_big = assetManagerController.setMaxRedeemedTickets([assetManager.address], maxRedeemedTickets_big, { from: governance });
+            let res_small = assetManagerController.setMaxRedeemedTickets([assetManager.address], maxRedeemedTickets_small, { from: governance });
+            let res_zero = assetManagerController.setMaxRedeemedTickets([assetManager.address], maxRedeemedTickets_zero, { from: governance });
+            
+            await expectRevert(res_big, "increase too big");
+            await expectRevert(res_small, "decrease too big");
+            await expectRevert(res_zero, "cannot be zero");
+        });
+
+        it("should update max redeemed tickets", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let maxRedeemedTickets_new = toBN(currentSettings.maxRedeemedTickets).muln(2);
+            let res = await assetManagerController.setMaxRedeemedTickets([assetManager.address], maxRedeemedTickets_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "maxRedeemedTickets", value: toBN(maxRedeemedTickets_new) });
+        });
+
+        it("should revert updating withdrawal wait when increase is too big or value is < 1", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let withdrawalWaitMinSeconds_big = toBN(currentSettings.withdrawalWaitMinSeconds).addn(11 * 60);
+            let withdrawalWaitMinSeconds_zero = 0;
+
+            let res_big = assetManagerController.setWithdrawalOrDestroyWaitMinSeconds([assetManager.address], withdrawalWaitMinSeconds_big, { from: governance });
+            let res_zero = assetManagerController.setWithdrawalOrDestroyWaitMinSeconds([assetManager.address], withdrawalWaitMinSeconds_zero, { from: governance });
+            
+            await expectRevert(res_big, "increase too big");
+            await expectRevert(res_zero, "cannot be zero");
+        });
+
+        it("should update withdrawal wait", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let withdrawalWaitMinSeconds_new = toBN(currentSettings.withdrawalWaitMinSeconds).muln(2);
+            let res = await assetManagerController.setWithdrawalOrDestroyWaitMinSeconds([assetManager.address], withdrawalWaitMinSeconds_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "withdrawalWaitMinSeconds", value: toBN(withdrawalWaitMinSeconds_new) });
+        });
+        
+        it("should revert updating ccb time when increase or decrease is too big", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let ccbTimeSeconds_big = toBN(currentSettings.ccbTimeSeconds).muln(3);
+            let ccbTimeSeconds_small = toBN(currentSettings.ccbTimeSeconds).divn(3);
+
+            let res_big = assetManagerController.setCcbTimeSeconds([assetManager.address], ccbTimeSeconds_big, { from: governance });
+            let res_small = assetManagerController.setCcbTimeSeconds([assetManager.address], ccbTimeSeconds_small, { from: governance });
+            
+            await expectRevert(res_big, "increase too big");
+            await expectRevert(res_small, "decrease too big");
+        });
+
+        it("should update ccb time", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let ccbTimeSeconds_new = toBN(currentSettings.ccbTimeSeconds).muln(2);
+            let res = await assetManagerController.setCcbTimeSeconds([assetManager.address], ccbTimeSeconds_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "ccbTimeSeconds", value: toBN(ccbTimeSeconds_new) });
+        });
+
+        it("should revert updating liquidation step when increase or decrease is too big", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let liquidationStepSeconds_big = toBN(currentSettings.liquidationStepSeconds).muln(3);
+            let liquidationStepSeconds_small = toBN(currentSettings.liquidationStepSeconds).divn(3);
+
+            let res_big = assetManagerController.setLiquidationStepSeconds([assetManager.address], liquidationStepSeconds_big, { from: governance });
+            let res_small = assetManagerController.setLiquidationStepSeconds([assetManager.address], liquidationStepSeconds_small, { from: governance });
+            
+            await expectRevert(res_big, "increase too big");
+            await expectRevert(res_small, "decrease too big");
+        });
+
+        it("should update liquidation step", async () => {
+            const currentSettings = await assetManager.getSettings();
+            let liquidationStepSeconds_new = toBN(currentSettings.liquidationStepSeconds).muln(2);
+            let res = await assetManagerController.setLiquidationStepSeconds([assetManager.address], liquidationStepSeconds_new, { from: governance });
+            expectEvent(res, "SettingChanged", { name: "liquidationStepSeconds", value: toBN(liquidationStepSeconds_new) });
         });
 
 
