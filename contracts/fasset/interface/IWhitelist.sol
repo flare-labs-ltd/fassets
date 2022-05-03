013df5886488e925100e0fa916b3a72f09379417
Urska Krivc <urska.krivc>
2022-05-03 12:55:47 +0200

    fix variable name typo

--- contracts/fasset/interface/IWhitelist.sol
+++ contracts/fasset/interface/IWhitelist.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.7.6 <0.9;
 
 
 interface IWhitelist {
-    function isWhitelisted(address _account) external view returns (bool);   
+    function isWhitelisted(address _address) external view returns (bool);   
 }
