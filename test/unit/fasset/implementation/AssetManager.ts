f5faf8dbe2b5062ec0796c1620a4a39a02f34716
kuco23 <nseverkar@gmail.com>
2022-05-23 15:00:42 +0200

    challenge agent

--- test/unit/fasset/implementation/AssetManager.ts
+++ test/unit/fasset/implementation/AssetManager.ts
@@ -13,6 +13,7 @@ import { setDefaultVPContract } from "../../../utils/token-test-helpers";
 import { SourceId } from "../../../utils/verification/sources/sources";
 import { assertWeb3DeepEqual, assertWeb3Equal, web3ResultStruct } from "../../../utils/web3assertions";
 import { createTestSettings } from "../test-settings";
+import { Challenger } from "../../../integration/utils/Challenger";
 
 const AgentVault = artifacts.require('AgentVault');
 const AttestationClient = artifacts.require('AttestationClientMock');
@@ -253,4 +254,18 @@ contract(`AssetManager.sol; ${getTestFile(__filename)}; Asset manager basic test
             assert.isFalse(await assetManager.paused());
         });
     });
+
+    describe("challenge agent", () => {
+
+        it.only("should challenge payments making free balance negative", async() => {
+            const agentVault = await createAgent(chain, agentOwner1, underlyingAgent1);
+            const txHash = await wallet.addTransaction(
+                underlyingAgent1, underlyingBurnAddr, 1, PaymentReference.addressOwnership(agentOwner1));
+            let proof = await attestationProvider.proveBalanceDecreasingTransaction(txHash, underlyingAgent1);
+            const res = await assetManager.freeBalanceNegativeChallenge(
+                [proof], agentVault.address, { from: whitelistedAccount });
+            expectEvent(res, 'UnderlyingFreeBalanceNegative', {agentVault: agentVault.address});
+        });
+    });
+
 });
