334e412fe5ce8bd4960fa2794d13247d1d1409a7
Iztok <iztok@abelium.com>
2022-07-06 17:17:58 +0200

    test that minting with payment before eoa proof is forbidden

--- test/integration/fasset-simulation/Audit.ts
+++ test/integration/fasset-simulation/Audit.ts
@@ -1,8 +1,10 @@
-import { expectRevert } from "@openzeppelin/test-helpers";
+import { expectRevert, time } from "@openzeppelin/test-helpers";
+import { PaymentReference } from "../../../lib/fasset/PaymentReference";
 import { EventArgs } from "../../../lib/utils/events/common";
+import { findRequiredEvent, requiredEventArgs } from "../../../lib/utils/events/truffle";
 import { toWei } from "../../../lib/utils/helpers";
 import { RedemptionRequested } from "../../../typechain-truffle/AssetManager";
-import { MockTransactionOptionsWithFee } from "../../utils/fasset/MockChain";
+import { MockChain, MockChainWallet, MockTransactionOptionsWithFee } from "../../utils/fasset/MockChain";
 import { getTestFile } from "../../utils/test-helpers";
 import { assertWeb3Equal } from "../../utils/web3assertions";
 import { Agent } from "../utils/Agent";
@@ -11,6 +13,8 @@ import { Challenger } from "../utils/Challenger";
 import { Minter } from "../utils/Minter";
 import { Redeemer } from "../utils/Redeemer";
 import { testChainInfo, testNatInfo } from "../utils/TestChainInfo";
+
+const AgentVault = artifacts.require('AgentVault');
 
 contract(`Audit.ts; ${getTestFile(__filename)}; Audit tests`, async accounts => {
     const governance = accounts[10];
@@ -27,6 +31,7 @@ contract(`Audit.ts; ${getTestFile(__filename)}; Audit tests`, async accounts => 
     // addresses on mock underlying chain can be any string, as long as it is unique
     const underlyingAgent1 = "Agent1";
     const underlyingAgent2 = "Agent2";
+    const underlyingOwner1 = "Owner1";
     const underlyingMinter1 = "Minter1";
     const underlyingMinter2 = "Minter2";
     const underlyingRedeemer1 = "Redeemer1";
@@ -40,7 +45,7 @@ contract(`Audit.ts; ${getTestFile(__filename)}; Audit tests`, async accounts => 
         context = await AssetContext.createTest(commonContext, testChainInfo.eth);
     });
 
-    it("must fail", async () => {
+    it("cannot use invalid payment reference type", async () => {
         const agent = await Agent.createTest(context, agentOwner1, underlyingAgent1);
         const minter = await Minter.createTest(context, minterAddress1, underlyingMinter1,
             context.underlyingAmount(10000));
@@ -75,7 +80,7 @@ contract(`Audit.ts; ${getTestFile(__filename)}; Audit tests`, async accounts => 
         await challenger.illegalPaymentChallenge(agent, fakeTxHash);
     });
 
-    it("must fail", async () => {
+    it("cannot abuse payment reference bits betwwen 64 and 192", async () => {
         const agent = await Agent.createTest(context, agentOwner1, underlyingAgent1);
         const minter = await Minter.createTest(context, minterAddress1, underlyingMinter1,
             context.underlyingAmount(10000));
@@ -108,5 +113,40 @@ contract(`Audit.ts; ${getTestFile(__filename)}; Audit tests`, async accounts => 
         await expectRevert(challenger.confirmActiveRedemptionPayment(request, tx1Hash, agent), "only agent vault owner");
         await expectRevert(challenger.illegalPaymentChallenge(agent, tx1Hash), "matching redemption active");
         await challenger.illegalPaymentChallenge(agent, fakeTxHash);
+    });
+    
+    it("should not be allowed to mint with the payment from before eoa proof", async () => {
+        if (!(context.chain instanceof MockChain)) assert.fail("only for mock chains");
+        // create mock wallet
+        const wallet = new MockChainWallet(context.chain);
+        // mint some underlying funds for the owner
+        const amount = context.underlyingAmount(10000);
+        context.chain.mint(underlyingOwner1, context.chain.requiredFee.muln(2).add(amount));
+        // deposit to underlying address
+        const guessBlock = Number(await time.latestBlock()) + 20;
+        const guessId = guessBlock % 1000 + 1;
+        const depositHash = await wallet.addTransaction(underlyingOwner1, underlyingAgent1, amount, PaymentReference.minting(guessId));
+        // create and prove transaction from underlyingAddress if EOA required
+        const eoaHash = await wallet.addTransaction(underlyingAgent1, underlyingOwner1, amount, PaymentReference.addressOwnership(agentOwner1));
+        const eoaProof = await context.attestationProvider.provePayment(eoaHash, underlyingAgent1, underlyingOwner1);
+        await context.assetManager.proveUnderlyingAddressEOA(eoaProof, { from: agentOwner1 });
+        // create agent
+        const response = await context.assetManager.createAgent(underlyingAgent1, { from: agentOwner1 });
+        const created = requiredEventArgs(response, 'AgentCreated');
+        const agentVault = await AgentVault.at(created.agentVault);
+        // create object
+        const agent = new Agent(context, agentOwner1, agentVault, underlyingAgent1, wallet);
+        // make agent available
+        const fullAgentCollateral = toWei(3e8);
+        await agent.depositCollateral(fullAgentCollateral);
+        await agent.makeAvailable(500, 2_2000);
+        // reserve collateral        
+        await time.advanceBlockTo(guessBlock - 1);
+        const minter = new Minter(context, agentOwner1, underlyingOwner1, wallet);
+        const crt = await minter.reserveCollateral(agent.agentVault.address, 1);
+        // mint
+        const proof = await context.attestationProvider.provePayment(depositHash, underlyingOwner1, crt.paymentAddress);
+        await expectRevert(context.assetManager.executeMinting(proof, crt.collateralReservationId, { from: agentOwner1 }),
+            "minting payment too old");
     });
 });
