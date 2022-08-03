bff78f06a88c221adbfad3079894fa59bd5852ad
Iztok <iztok@abelium.com>
2022-08-03 13:37:59 +0200

    use governance settings

--- deployment/lib/asset-manager-controller-parameters.ts
+++ deployment/lib/asset-manager-controller-parameters.ts
@@ -3,6 +3,13 @@ export interface CommonParameters {
      * The governance address used during deploy.
      */
     initialGovernance: string;
+    
+    /**
+     * Address for the governance settings contract to be used in production mode.
+     * Will be a genesis contract 0x1000000000000000000000000000000000000007 on Flare and 
+     * a deployed contract address on Songbird.
+     */
+    governanceSettings: string;
 
     /**
      * Address of the address updater contract.
---
+++ deployment/lib/deploy-asset-manager.ts
@@ -0,0 +1,9 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { linkContracts } from "./link-contracts";
+
+export async function deployAssetManager(hre: HardhatRuntimeEnvironment, configName: string) {
+    
+    // await linkContracts(hre, ["AssetManager"], `deployment/deploys/${configName}.libs.json`);
+    
+    
+}
