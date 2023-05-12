9e3c57f479361be76d251f8a46449fa51f4332b3
Iztok <iztok@abelium.com>
2023-05-12 15:10:21 +0200

    doc comments in IAgentVault

--- contracts/userInterfaces/IAgentVault.sol
+++ contracts/userInterfaces/IAgentVault.sol
@@ -9,48 +9,93 @@ import "flare-smart-contracts/contracts/userInterfaces/IDistributionToDelegators
 import "./ICollateralPool.sol";
 
 interface IAgentVault {
-    // without "onlyOwner" to allow owner to send funds from any source
-    function buyCollateralPoolTokens() external payable;
-
-    // only owner
-    function withdrawPoolFees(uint256 _amount, address _recipient) external;
-
-    // only owner
-    function redeemCollateralPoolTokens(uint256 _amount, address payable _recipient) external;
-
-    // must call `token.approve(vault, amount)` before for each token in _tokens
-    // without "onlyOwner" to allow owner to send funds from any source
+    /**
+     * Deposit class1 collateral.
+     * Parameter `_token` is explicit to allow depositing before collateral switch.
+     * NOTE: owner must call `token.approve(vault, amount)` before calling this method.
+     * NOTE: anybody can call this method, to allow owner to deposit from any wallet.
+     */
     function depositCollateral(IERC20 _token, uint256 _amount) external;
 
-    // update collateral after `transfer(vault, some amount)` was called (alternative to depositCollateral)
-    // without "onlyOwner" to allow owner to send funds from any source
+    /**
+     * Update collateral after `transfer(vault, some amount)` was called (alternative to depositCollateral).
+     * Parameter `_token` is explicit to allow depositing before collateral switch.
+     * NOTE: anybody can call this method, to allow owner to deposit from any source.
+     */
     function collateralDeposited(IERC20 _token) external;
 
-    // only owner
+    /**
+     * Withdraw class1 collateral. This method will work for any token, but for class1 and agent pool tokens
+     * (which are loked because they may be backing f-assets) there is a check that there was prior announcement
+     * by calling `assetManager.announceClass1CollateralWithdrawal(...)`.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function withdrawCollateral(IERC20 _token, uint256 _amount, address _recipient) external;
 
-    // Allow transferring a token, airdropped to the agent vault, to the owner (cold wallet).
-    // Doesn't work for collateral tokens because this would allow withdrawing the locked collateral.
-    // only owner
+    /**
+     * Allow transferring a token, airdropped to the agent vault, to the owner (cold wallet).
+     * Doesn't work for class1 collateral tokens or agent's pool tokens  because this would allow
+     * withdrawing the locked collateral.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function transferExternalToken(IERC20 _token, uint256 _amount) external;
 
-    // only owner
+    /**
+     * Buy collateral pool tokens for NAT.
+     * Holding enough pool tokens in the vault is required for minting.
+     * NOTE: anybody can call this method, to allow owner to deposit from any source.
+     */
+    function buyCollateralPoolTokens() external payable;
+
+    /**
+     * Collateral pool tokens which must be held by the agent accrue minting fees in form of f-assets.
+     * These fees can be withdrawn using this method.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
+    function withdrawPoolFees(uint256 _amount, address _recipient) external;
+
+    /**
+     * This method allows the agent to convert collateral pool tokens back to NAT.
+     * Prior announcement is required by calling `assetManager.announceAgentPoolTokenRedemption(...)`.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
+    function redeemCollateralPoolTokens(uint256 _amount, address payable _recipient) external;
+
+    /**
+     * Delegate FTSO vote power for a collateral token held in this vault.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function delegate(IVPToken _token, address _to, uint256 _bips) external;
 
-    // only owner
+    /**
+     * Undelegate FTSO vote power for a collateral token held in this vault.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function undelegateAll(IVPToken _token) external;
 
-    // only owner
+    /**
+     * Revoke FTSO vote power delegation for a block in the past for a collateral token held in this vault.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function revokeDelegationAt(IVPToken _token, address _who, uint256 _blockNumber) external;
 
-    // only owner
+    /**
+     * Delegate governance vote power for possible NAT collateral token held in this vault.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function delegateGovernance(address _to) external;
 
-    // only owner
+    /**
+     * Undelegate governance vote power for possible NAT collateral token held in this vault.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function undelegateGovernance() external;
 
-    // Claim ftso rewards. Alternatively, you can set claim executor and then claim directly from FtsoRewardManager.
-    // only owner
+    /**
+     * Claim ftso rewards earned by delegating.
+     * Alternatively, you can set claim executor and then claim directly from FtsoRewardManager.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function claimFtsoRewards(
         IFtsoRewardManager _ftsoRewardManager,
         uint256 _lastRewardEpoch,
@@ -58,15 +103,20 @@ interface IAgentVault {
     ) external
         returns (uint256);
 
-    // Set executors and recipients that can then automatically claim rewards through FtsoRewardManager.
-    // only owner
+    /**
+     * Set executors and recipients that can then automatically claim rewards through FtsoRewardManager.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function setFtsoAutoClaiming(
         IClaimSetupManager _claimSetupManager,
         address[] memory _executors,
         address[] memory _allowedRecipients
     ) external payable;
 
-    // only owner
+    /**
+     * Claim airdrops earned by holding wNAT in the vault.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function claimAirdropDistribution(
         IDistributionToDelegators _distribution,
         uint256 _month,
@@ -74,11 +124,18 @@ interface IAgentVault {
     ) external
         returns(uint256);
 
-    // only owner
+    /**
+     * Opt out of airdrops for wNAT in the vault.
+     * NOTE: only the owner of the agent vault may call this method.
+     */
     function optOutOfAirdrop(
         IDistributionToDelegators _distribution
     ) external;
 
+    /**
+     * Get the address of the collateral pool contract corresponding to this agent vault
+     * (there is 1:1 correspondence between agent vault and collateral pools).
+     */
     function collateralPool()
         external view
         returns (ICollateralPool);
