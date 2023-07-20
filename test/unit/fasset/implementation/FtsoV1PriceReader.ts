9e85ee836c8433a1ca3d109560804a311e7c93c9
Iztok <iztok@abelium.com>
2023-07-20 10:40:51 +0200

    coverage for FtsoV1PriceReader

---
+++ test/unit/fasset/implementation/FtsoV1PriceReader.ts
@@ -0,0 +1,71 @@
+import { constants, expectRevert, time } from "@openzeppelin/test-helpers";
+import { erc165InterfaceId } from "../../../../lib/utils/helpers";
+import { FtsoV1PriceReaderInstance, IERC165Contract, IERC20Contract, IICleanableContract, IIVPTokenContract, IVPTokenContract } from "../../../../typechain-truffle";
+import { getTestFile, loadFixtureCopyVars } from "../../../utils/test-helpers";
+import { assertWeb3Equal } from "../../../utils/web3assertions";
+import { TestFtsos, TestSettingsContracts, createTestContracts, createTestFtsos } from "../../../utils/test-settings";
+import { testChainInfo } from "../../../integration/utils/TestChainInfo";
+
+const FtsoV1PriceReader = artifacts.require('FtsoV1PriceReader');
+
+contract(`FAsset.sol; ${getTestFile(__filename)}; FAsset basic tests`, async accounts => {
+    let contracts: TestSettingsContracts;
+    let ftsos: TestFtsos;
+    let priceReader: FtsoV1PriceReaderInstance;
+    const governance = accounts[10];
+
+    async function initialize() {
+        contracts = await createTestContracts(governance);
+        priceReader = await FtsoV1PriceReader.new(contracts.addressUpdater.address, contracts.ftsoRegistry.address);
+        ftsos = await createTestFtsos(contracts.ftsoRegistry, testChainInfo.xrp);
+        return { contracts, priceReader, ftsos };
+    }
+
+    beforeEach(async () => {
+        ({ contracts, priceReader, ftsos } = await loadFixtureCopyVars(initialize));
+    });
+
+    describe("method tests", () => {
+        it("should require nonzero ftsoRegistry", async () => {
+            const pr = FtsoV1PriceReader.new(contracts.addressUpdater.address, constants.ZERO_ADDRESS);
+            await expectRevert(pr, "zero address");
+        });
+
+        it("should get price", async () => {
+            await ftsos.usdc.setCurrentPrice(123456, 10);
+            const { 0: price, 1: timestamp, 2: decimals } = await priceReader.getPrice("USDC");
+            assertWeb3Equal(price, 123456);
+            assertWeb3Equal(timestamp, (await time.latest()).subn(10));
+            assertWeb3Equal(decimals, 5);
+        });
+
+        it("should get trusted price", async () => {
+            await ftsos.usdc.setCurrentPriceFromTrustedProviders(123456, 10);
+            const { 0: price, 1: timestamp, 2: decimals } = await priceReader.getPriceFromTrustedProviders("USDC");
+            assertWeb3Equal(price, 123456);
+            assertWeb3Equal(timestamp, (await time.latest()).subn(10));
+            assertWeb3Equal(decimals, 5);
+        });
+
+        it("should update contract addresses", async () => {
+            await contracts.addressUpdater.update(["AddressUpdater", "FtsoRegistry"],
+                [accounts[79], accounts[80]],
+                [priceReader.address],
+                { from: governance });
+            assert.equal(await priceReader.getAddressUpdater(), accounts[79]);
+            assert.equal(await priceReader.ftsoRegistry(), accounts[80]);
+        });
+    });
+
+    describe("ERC-165 interface identification", () => {
+        it("should properly respond to supportsInterface", async () => {
+            const IERC165 = artifacts.require("@openzeppelin/contracts/utils/introspection/IERC165.sol:IERC165" as any) as any as IERC165Contract;
+            const IPriceReader = artifacts.require("IPriceReader");
+            const iERC165 = await IERC165.at(priceReader.address);
+            const iPriceReader = await IPriceReader.at(priceReader.address);
+            assert.isTrue(await priceReader.supportsInterface(erc165InterfaceId(iERC165.abi)));
+            assert.isTrue(await priceReader.supportsInterface(erc165InterfaceId(iPriceReader.abi)));
+            assert.isFalse(await priceReader.supportsInterface('0xFFFFFFFF'));  // must not support invalid interface
+        });
+    });
+});
