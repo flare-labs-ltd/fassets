d3aa7ddd08a00f1dc1c2bbd1863a36858fb2eb32
Iztok <iztok@abelium.com>
2024-05-24 14:04:37 +0200

    events in asset manager abi

--- contracts/fasset/implementation/AssetManager.sol
+++ contracts/fasset/implementation/AssetManager.sol
@@ -3,13 +3,15 @@ pragma solidity 0.8.23;
 
 import "../../diamond/implementation/Diamond.sol";
 import "../../diamond/library/LibDiamond.sol";
-
+import "../../userInterfaces/IAssetManagerEvents.sol";
 
 /**
  * The contract that can mint and burn f-assets while managing collateral and backing funds.
  * There is one instance of AssetManager per f-asset type.
  */
-contract AssetManager is Diamond {
+contract AssetManager is Diamond, IAssetManagerEvents {
+    // IAssetManagerEvents interface is included so that blockchain explorers will be able
+    // to decode events for verified AssetManager instances.
     constructor(IDiamondCut.FacetCut[] memory _diamondCut, address _init, bytes memory _initCalldata) payable {
         LibDiamond.diamondCut(_diamondCut, _init, _initCalldata);
     }
