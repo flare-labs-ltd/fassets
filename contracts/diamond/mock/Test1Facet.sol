475fa62736f0e746c79cde4006fa17063c3799f3
Iztok <iztok@abelium.com>
2024-10-01 11:23:55 +0200

    lint warning fix

--- contracts/diamond/mock/Test1Facet.sol
+++ contracts/diamond/mock/Test1Facet.sol
@@ -1,35 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { TestLib } from "./TestLib.sol";
+
 // Example library to show a simple example of diamond storage
-
-// solhint-disable no-inline-assembly
-library TestLib {
-
-  bytes32 internal constant DIAMOND_STORAGE_POSITION = keccak256("diamond.standard.test.storage");
-
-  struct TestState {
-      address myAddress;
-      uint256 myNum;
-  }
-
-  function diamondStorage() internal pure returns (TestState storage ds) {
-      bytes32 position = DIAMOND_STORAGE_POSITION;
-      assembly {
-          ds.slot := position
-      }
-  }
-
-  function setMyAddress(address _myAddress) internal {
-    TestState storage testState = diamondStorage();
-    testState.myAddress = _myAddress;
-  }
-
-  function getMyAddress() internal view returns (address) {
-    TestState storage testState = diamondStorage();
-    return testState.myAddress;
-  }
-}
 
 contract Test1Facet {
     event TestEvent(address something);
