4907a1633aad0538045af3931344c73c9988036f
Iztok <iztok@abelium.com>
2024-11-05 16:26:37 +0100

    fdc request fee mock

--- contracts/fdc/mock/FdcHubMock.sol
+++ contracts/fdc/mock/FdcHubMock.sol
@@ -2,9 +2,17 @@
 pragma solidity 0.8.23;
 
 import "flare-smart-contracts-v2/contracts/userInterfaces/IFdcHub.sol";
+import "./FdcRequestFeeConfigurationsMock.sol";
 
 
 contract FdcHubMock is IFdcHub {
+    /// The FDC request fee configurations contract.
+    IFdcRequestFeeConfigurations public fdcRequestFeeConfigurations;
+
+    constructor() {
+        fdcRequestFeeConfigurations = new FdcRequestFeeConfigurationsMock();
+    }
+
      /**
      * Method to request an attestation.
      * @param _data ABI encoded attestation request
