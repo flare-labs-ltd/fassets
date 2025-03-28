0f095cca0240ad498326972acc49f0a1e026e037
Iztok <iztok@abelium.com>
2025-03-28 08:57:05 +0100

    small refactor in transfers

--- contracts/utils/lib/Transfers.sol
+++ contracts/utils/lib/Transfers.sol
@@ -5,7 +5,13 @@ import "../../openzeppelin/library/Reentrancy.sol";
 
 
 library Transfers {
-    uint256 internal constant TRANSFER_GAS_ALLOWANCE = 100000;
+    uint256 internal constant TRANSFER_GAS_ALLOWANCE = 100_000;
+
+    // make sure the transfer is only called in non-reentrant method
+    modifier requireReentrancyGuard {
+        Reentrancy.requireReentrancyGuard();
+        _;
+    }
 
     /**
      * Transfer the given amount of NAT to recipient without gas limit of `address.transfer()`.
@@ -18,9 +24,10 @@ library Transfers {
      * @param _recipient the recipient address
      * @param _amount the amount in NAT Wei
      */
-    function transferNAT(address payable _recipient, uint256 _amount) internal {
-        // make sure the transfer is only called in non-reentrant method
-        Reentrancy.requireReentrancyGuard();
+    function transferNAT(address payable _recipient, uint256 _amount)
+        internal
+        requireReentrancyGuard
+    {
         if (_amount > 0) {
             /* solhint-disable avoid-low-level-calls */
             //slither-disable-next-line arbitrary-send-eth
@@ -39,9 +46,11 @@ library Transfers {
      * @param _recipient the recipient address
      * @param _amount the amount in NAT Wei
      */
-    function transferNATAllowFailure(address payable _recipient, uint256 _amount) internal returns (bool) {
-        // make sure the transfer is only called in non-reentrant method
-        Reentrancy.requireReentrancyGuard();
+    function transferNATAllowFailure(address payable _recipient, uint256 _amount)
+        internal
+        requireReentrancyGuard
+        returns (bool)
+    {
         if (_amount > 0) {
             /* solhint-disable avoid-low-level-calls */
             //slither-disable-next-line arbitrary-send-eth
