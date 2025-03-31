6d82c0285c3539f16b46ec3898076584007c54d7
Iztok <iztok@abelium.com>
2025-03-31 17:47:01 +0200

    staging core vault parameter fixes

---
+++ deployment/test/scripts/update-staging-params.ts
@@ -0,0 +1,23 @@
+import hre from "hardhat";
+import { runAsyncMain } from "../../../lib/utils/helpers";
+import { FAssetContractStore } from "../../lib/contracts";
+import { loadDeployAccounts, networkConfigName, waitFinalize } from "../../lib/deploy-utils";
+
+const AssetManager = artifacts.require("IIAssetManager");
+
+runAsyncMain(async () => {
+    const networkConfig = networkConfigName(hre);
+    const contracts = new FAssetContractStore(`deployment/deploys/${networkConfig}.json`, false);
+    const { deployer } = loadDeployAccounts(hre);
+
+    const managerNames = ["AssetManager_FTestBTC", "AssetManager_FTestDOGE", "AssetManager_FTestXRP", "AssetManager_FSimCoinX"];
+    // const managerNames = ["AssetManager_FTestXRP"];
+
+    for (const name of managerNames) {
+        const address = contracts.getAddress(name);
+        const am = await AssetManager.at(address);
+        // await waitFinalize(hre, deployer, () => am.setCoreVaultTransferTimeExtensionSeconds(7200, { from: deployer }));
+        // await waitFinalize(hre, deployer, () => am.setCoreVaultRedemptionFeeBIPS(0, { from: deployer }));
+        // await waitFinalize(hre, deployer, () => am.setCoreVaultMinimumAmountLeftBIPS(2000, { from: deployer }));
+    }
+});
